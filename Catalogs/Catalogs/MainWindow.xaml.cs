d8f8db2402b3c2238c12c611cb4455996a98f905
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-09 22:00:19 +0100

    budowa katalogów

--- Catalogs/Catalogs/MainWindow.xaml.cs
+++ Catalogs/Catalogs/MainWindow.xaml.cs
@@ -24,5 +24,255 @@ namespace Catalogs
         {
             InitializeComponent();
         }
+
+        private void Imageaa_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("ok");
+        }
+
+        private void Imageca_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("ok");
+        }
+
+        private void Imageba_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageja_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageia_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageha_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagega_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagefa_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageea_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageda_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageab_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagebb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagecb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagejb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageib_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagehb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagegb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagefb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageeb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagedb_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageac_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagecc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagebc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagejc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageic_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagehc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagegc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagefc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageec_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageje_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagedc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagead_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagecd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagebd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagejd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagehd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagegd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagefd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageed_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagedd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageae_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagece_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagebe_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageie_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagehe_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagege_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagefe_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imageee_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Imagede_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
